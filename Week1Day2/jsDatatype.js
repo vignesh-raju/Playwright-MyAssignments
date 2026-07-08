6c7c69eaffaba17b74bc33974a3269e0fd541bba
vignesh-raju <60388251+vignesh-raju@users.noreply.github.com>
2026-07-08 15:18:50 +0530

    Add files via upload
    Day 2

---
+++ Week1Day2/jsDatatype.js
@@ -0,0 +1,28 @@
+
+let firstName = "Vignesh"
+console.log(firstName);
+console.log(typeof firstName);
+
+let companyName = "Testleaf"
+console.log(companyName);
+console.log(typeof companyName);
+
+let mobileNumber = 9500671681
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+let isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+let hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+
+
+
+
+
+
+
+
