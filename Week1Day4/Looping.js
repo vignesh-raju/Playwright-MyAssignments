e3d6c74090330f818ef161c54f71e9418285db03
vignesh-raju <60388251+vignesh-raju@users.noreply.github.com>
2026-07-12 19:53:18 +0530

    Add files via upload

---
+++ Week1Day4/Looping.js
@@ -0,0 +1,10 @@
+
+for (let i = 1; i <= 25; i++) {
+
+    if(i%2 == 0){
+        console.log("The value",i,"is an even number");
+    }else{
+        console.log("The value",i,"is an odd number");
+    }
+    
+}
